c56d08c2d927831ae305298ed2f924976dd50893
Orest <orest.lozynsky@groupdocs.com>
2012-10-29 04:55:00 +0200

    Textpattern Ver.0.1.0

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-textpattern-groupdocs-viewer-source
-===================================
+GROUPDOCS VIEWER
 
-GroupDocs Viewer plugin for TextPattern CMS  (Source code)
+GroupDocs Viewer plugin for Textpattern
+
+With GroupDocs Viewer plugin for Textpattern you can easily view on website PDF's, Word documents, Excel documents, Powerpoint documents and more with the GroupDocs Viewer tool, directly from within your Textpattern website.
+
+INSTALLATION
+
+	1. To install: textpattern > admin > plugins
+	2. Paste the following text into the 'Install plugin' box:
+
+Sign, Manage, Annotate, Assemble, Compare and Convert Documents with GroupDocs
+
+	Annotate PDF, Word, Excel, Powerpoint and Images with GroupDocs Viewer
+	Download Viewer plugin package here
+	Embed DOC, DOCX, PDF Viewer in your Textpattern website
+	See source code for GroupDocs Viewer plugin for Textpattern
+
+Created by GroupDocs Marketplace Team.
---
+++ gdv_groupdocs_viewer_v0.1.php
@@ -0,0 +1,112 @@
+<?php
+
+// This is a PLUGIN TEMPLATE.
+// .....................................................................
+// This is a plugin for Textpattern - http://textpattern.com/
+// To install: textpattern > admin > plugins
+// Paste the following text into the 'Install plugin' box:
+// .....................................................................
+
+// Copy this file to a new name like abc_myplugin.php.  Edit the code, then
+// run this file at the command line to produce a plugin for distribution:
+// $ php abc_myplugin.php > abc_myplugin-0.1.txt
+
+// Plugin name is optional.  If unset, it will be extracted from the current
+// file name. Plugin names should start with a three letter prefix which is
+// unique and reserved for each plugin author ("abc" is just an example).
+// Uncomment and edit this line to override:
+$plugin['name'] = 'gdv_groupdocs_viewer';
+
+// Allow raw HTML help, as opposed to Textile.
+// 0 = Plugin help is in Textile format, no raw HTML allowed (default).
+// 1 = Plugin help is in raw HTML.  Not recommended.
+# $plugin['allow_html_help'] = 1;
+
+$plugin['version'] = '0.1';
+$plugin['author'] = 'Orest Loginsky';
+$plugin['author_uri'] = 'groupdocs.com';
+$plugin['description'] = 'GroupDocs is a next generation Document Management solution that makes it easier for businesses to collaborate, share and work with documents online. So, organise, view, annotate, compare, assemble and share all your documents with Textpattern';
+
+// Plugin load order:
+// The default value of 5 would fit most plugins, while for instance comment
+// spam evaluators or URL redirectors would probably want to run earlier
+// (1...4) to prepare the environment for everything else that follows.
+// Values 6...9 should be considered for plugins which would work late.
+// This order is user-overrideable.
+$plugin['order'] = '5';
+
+// Plugin 'type' defines where the plugin is loaded
+// 0 = public       : only on the public side of the website (default)
+// 1 = public+admin : on both the public and admin side
+// 2 = library      : only when include_plugin() or require_plugin() is called
+// 3 = admin        : only on the admin side
+$plugin['type'] = '3';
+
+// Plugin "flags" signal the presence of optional capabilities to the core plugin loader.
+// Use an appropriately OR-ed combination of these flags.
+// The four high-order bits 0xf000 are available for this plugin's private use
+if (!defined('PLUGIN_HAS_PREFS')) define('PLUGIN_HAS_PREFS', 0x0001); // This plugin wants to receive "plugin_prefs.{$plugin['name']}" events
+if (!defined('PLUGIN_LIFECYCLE_NOTIFY')) define('PLUGIN_LIFECYCLE_NOTIFY', 0x0002); // This plugin wants to receive "plugin_lifecycle.{$plugin['name']}" events
+
+$plugin['flags'] = '0';
+
+if (!defined('txpinterface'))
+        @include_once('zem_tpl.php');
+
+# --- BEGIN PLUGIN CODE ---
+/**
+ * GroupDocs Viewer Plugin v0.1.0
+ * Author: Orest Loginsky
+ * Date: 26.10.2012
+ * 
+ * By default: Textpattern doesn't have WYSIWYG Editor (ex.: TinyMCE), means that you will be seeing 
+ * only <iframe> tag while editing page. See results in "Article preview" or your site.
+ */
+
+// admin user only
+if (@txpinterface == 'admin') {
+    add_privs('article', '1'); // Publishers only
+    register_callback('gdv_append_button', 'article_ui', 'title');
+}
+
+// add GroupDocs button
+function gdv_append_button($event, $step, $data, $rs) {
+    
+    $js= gdv_javascript();
+    $button = '<input type="button" value="Insert GroupDocs File ID" onclick="gdv_insert_fileid()">';
+    $output_result = isset($rs['url_title']) ? '<br/>' . $js.$button : '';
+    return $data.$output_result;
+}
+
+// add javascript
+function gdv_javascript(){
+    // jquery is working here
+    $r= '<script>';
+    $r.= 'function gdv_insert_fileid(){
+        
+            // Enter GroupDocs File ID
+            var ans=prompt("Enter GroupDocs File ID:","");
+            if(ans.length<50) { alert("Sorry, but this File ID is too short"); return false; }
+            if(ans.length>70) { alert("Sorry, but this File ID is too big"); return false; }
+
+            var gdv_iframe = \'<iframe src="https://apps.groupdocs.com/document-viewer/embed/\'+ans+\'" frameborder="0" width="600" height="400"></iframe>\';
+            
+            // insert in the end of <textarea id="body">
+            var gdv_body = $("textarea#body").html()+gdv_iframe;
+            $("textarea#body").html(gdv_body);
+    }';
+    $r.= '</script>';
+    return $r;
+}
+
+# --- END PLUGIN CODE ---
+if (0) {
+?>
+<!--
+# --- BEGIN PLUGIN HELP ---
+
+# --- END PLUGIN HELP ---
+-->
+<?php
+}
+?>
